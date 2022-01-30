db23c646bc32ff7e014e4afb27d5133374f467fa
Gerlond <vasya.dik1999@gmail.com>
2022-01-30 21:01:38 +0300

    commit

--- challenge.js
+++ challenge.js
@@ -38,7 +38,7 @@ Steven is still building his tip calculator, using the same rules as before: Tip
 4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
 
 TEST DATA: 125, 555 and 44
-
+*/
 
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
@@ -56,14 +56,16 @@ for (i = 0; i < bill.length; i++) {
   sum.push(bill[i] + tip[i]);
 }
 console.log(sum);
-*/
+
 //challenge
 /*
+
 Let's improve Steven's tip calculator even more, this time using loops!
 
 1. Create an array 'bills' containing all 10 test bill values
 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
-3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total 
+values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
 
 TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 */
